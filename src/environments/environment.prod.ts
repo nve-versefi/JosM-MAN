db200fe57baece08c0280243a681eece35f6ffc9
nve-versefi <n.vaneerde@versefi.co.uk>
2024-03-16 19:29:09 +0000

    Change env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://josm-backend-6599f0686bc6.herokuapp.com' , // URL del servidor de desarrollo
+    apiUrl: 'https://jos-m-man.vercel.app/api' , // URL del servidor de desarrollo
   };
